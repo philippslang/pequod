e363622ea2d8afaf92a560217914a91de979fd58
ClaraCL <clarita.castellanos@gmail.com>
2017-02-22 17:20:55 +0000

    clara:  interpreter

--- interpreter/interpret.py
+++ interpreter/interpret.py
@@ -5,10 +5,13 @@ import itertools
 #from google.cloud import speech as google_speech
 
 from googleapiclient import discovery
-import httplib2
+import httplib2, sys, json
 from oauth2client.client import GoogleCredentials
 
 import mysite.dispatch as internal_requests
+import googleapiclient.discovery
+from autocorrectdict import context_correct_dict
+from syndict import synonym_dict
 
 # [START authenticating]
 DISCOVERY_URL = ('https://{api}.googleapis.com/$discovery/rest?'
@@ -26,6 +29,14 @@ def get_speech_service():
     return discovery.build('speech', 'v1beta1', http=http, discoveryServiceUrl=DISCOVERY_URL)
 # [END authenticating]
 
+
+def get_native_encoding_type():
+    """Returns the encoding type that matches Python's native strings."""
+    if sys.maxunicode == 65535:
+        return 'UTF16'
+    else:
+        return 'UTF32'
+        
 
 def supported_queries_words(supported_queries):
     """
@@ -104,3 +115,146 @@ def interpret(base64_audio, supported_queries):
 
     print 'INTERPRETER::interpret::interpret: Got empty audio, defaulting to ', interpretation        
     return interpretation
+
+
+def keywordmatch(query_list,tokens):
+    
+    """
+    >>>keywordmatch3(['oil in place','recovery','oil','place'])
+    """
+    
+    #Find the query that best matches
+    maxlen=0
+    best_ix=-1
+    for ii,stand_query in enumerate(query_list):
+        querytoken=stand_query.split()
+        if len(set(tokens).intersection(querytoken))>maxlen:
+            best_ix=ii
+            maxlen=len(set(tokens).intersection(querytoken))
+            print(querytoken, tokens,maxlen,best_ix)
+            
+    print(maxlen,best_ix,query_list[best_ix])       
+    print('End of popular query list')
+    
+    if best_ix == -1 :
+        #match with synonym   , if availablle
+        for ii,stand_query in enumerate(query_list):
+            querytoken=stand_query.split()
+            if querytoken in synonym_dict.keys() :
+                for synonyms in synonym_dict[querytoken]:
+                    if len(set(tokens).intersection(synonyms))>maxlen:
+                        best_ix=ii
+                        maxlen=len(set(tokens).intersection(synonyms))
+    if best_ix==-1:
+        return internal_requests.BAD_VALUE
+    else:
+        return query_list[best_ix]
+
+
+def autocorrect(list_words):
+    """
+    >>autocorrect(['face'])
+    phase
+    """
+    for ii,word in enumerate(list_words):
+        if word in context_correct_dict.keys():
+            list_words[ii]=context_correct_dict[word]
+                             
+    return list_words
+    
+def rmvstopwords(sentence2):
+    """
+    >>rmvstopwords('This is a test with an error')
+    ['error']
+    """
+    sentence=sentence2.lower()
+    mystopwords=[u'all', u'just', u'being', u'over', u'both', u'through', u'yourselves', u'its', u'before', u'o', u'hadn', u'herself', u'll', u'had', u'should', u'to', u'only', u'won', u'under', u'ours', u'has', u'do', u'them', u'his', u'very', u'they', u'not', u'during',
+ u'now', u'him', u'nor', u'd', u'did', u'didn', u'this', u'she', u'each', u'further', u'where', u'few', u'because', u'doing', u'some', u'hasn', u'are', u'our', u'ourselves', u'out', u'what', u'for', u'while', u're', u'does', u'above', u'between', u'mustn', u't', u'be', u'we', u'who', u'were', u'here', u'shouldn', u'hers', u'by', u'on', u'about', u'couldn', u'of', u'against', u's', u'isn', u'or', u'own', u'into', u'yourself', u'down', u'mightn', u'wasn', u'your', u'from', u'her', u'their', u'aren', u'there', u'been', u'whom', u'too', u'wouldn', u'themselves', u'weren', u'was', u'until', u'more', u'himself', u'that', u'but', u'don', u'with', u'than', u'those', u'he', u'me', u'myself', u'ma', u'these', u'up', u'will', u'below', u'ain', u'can', u'theirs', u'my', u'and', u've', u'then', u'is', u'am', u'it', u'doesn', u'an', u'as', u'itself', u'at', u'have', u'in', u'any', u'if', u'again', u'no', u'when', u'same', u'how', u'other', u'which', u'you', u'shan', u'needn', u'haven', u'after', u'most', u'such', u'why', u'a', u'off', u'i', u'm', u'yours', u'so', u'y', u'the', u'having', u'once']
+ 
+    redundantwds=['simulation','code','file','test']
+    interrogativewords=['did','how','when','which','why','where']
+    
+    tokens=sentence.split()
+
+    filtered_words = [word for word in tokens if word not in (mystopwords+redundantwds+interrogativewords)]
+    return filtered_words
+
+def match(sentence, options):
+    """
+    Use like
+    >>> match('is there an error', ['cell count', 'error count', 'oil in place'])
+    """
+    for option in options:
+        intersection = set(sentence.split()).intersection(option.split())
+        if intersection: #stops at first non null
+            return next(iter(intersection))
+    return internal_requests.BAD_VALUE
+
+def match2(sentence, options):
+    """
+    Use like
+    >>> match('is there an error', ['cell count', 'error count', 'oil in place'])
+    """
+    allmatches=[]
+    lenmatched=[]
+    ngrammax=0
+    for option in options:
+        intersection = set(sentence.split()).intersection(option.split())
+        if intersection: #stops at first non null
+            if len(intersection)>ngrammax: 
+                allmatches=intersection
+                ngrammax= len(intersection)
+            #return option
+    print allmatches
+    print lenmatched
+    if not allmatches:
+        return internal_requests.BAD_VALUE
+    else:
+        return allmatches
+        
+        
+        
+def convertbasic(sentence):
+    """
+    >>> convert_basic('were there any errors')
+    ['are, is, any, error']    
+    """
+    body = {
+        'document': {
+            'type': 'PLAIN_TEXT',
+            'content': sentence,
+        },
+        'encoding_type': get_native_encoding_type(),
+    }
+    service = googleapiclient.discovery.build('language', 'v1')
+
+    request = service.documents().analyzeSyntax(body=body)
+    response = request.execute()
+    tokens_basic=[str(response['tokens'][ii]['lemma']) for ii in range(len(sentence.split()))   ]   
+    return tokens_basic
+    
+    
+def nouns(sentence):
+    """
+    >>> nouns('is there an error')
+    ['error']
+    """
+    body = {
+        'document': {
+            'type': 'PLAIN_TEXT',
+            'content': sentence,
+        },
+        'encoding_type': get_native_encoding_type(),
+    }
+
+    service = googleapiclient.discovery.build('language', 'v1')
+
+    request = service.documents().analyzeSyntax(body=body)
+    response = request.execute()
+    #parsed = json.loads(response)
+    #for ii in range(4):
+    #    print response['tokens'][ii]['lemma']
+    #print len(response['tokens'][0])#['lemma']
+    print json.dumps(response, indent=4, sort_keys=True)
+    
+    return sentence[0]
