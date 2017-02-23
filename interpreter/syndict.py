70afc10f3cf9614884d83db876fc7507487d010e
ClaraCL <clarita.castellanos@gmail.com>
2017-02-23 14:17:14 +0000

    dictionary of synonyms

--- interpreter/syndict.py
+++ interpreter/syndict.py
@@ -2,16 +2,16 @@
 synonym_dict={}
 
 
-{'error': [u'mistake', u'fail',u'failure', u'fault', u'erroneousness', u'erroneous_belief', u'misplay', u'wrongdoing', u'computer_error']}
+synonym_dict['error']=[u'mistake', u'fail',u'failure', u'fault', u'erroneousness', u'erroneous_belief', u'misplay', u'wrongdoing', u'computer_error', u'issue']
 
+synonym_dict['finish']=[u'coating', u'finished', u'finishing', u'stopping_point', u'finale', u'finis', u'last', u'conclusion', u'close', u'polish', u'refinement', u'culture', u'cultivation', u'destination', u'goal', u'ending', u'complete', u'finish_up', u'land_up', u'fetch_up', u'end_up', u'wind_up', u'end', u'stop', u'terminate', u'cease', u'eat_up', u'polish_off']
 
-{'finish': [u'coating', u'finish', u'finishing', u'stopping_point', u'finale', u'finis', u'last', u'conclusion', u'close', u'polish', u'refinement', u'culture', u'cultivation', u'destination', u'goal', u'ending', u'complete', u'finish_up', u'land_up', u'fetch_up', u'end_up', u'wind_up', u'end', u'stop', u'terminate', u'cease', u'eat_up', u'polish_off']}
+synonym_dict['rate']=[u'rate', u'charge_per_unit', u'pace', u'rank', u'range', u'order', u'grade', u'place', u'value']
 
-{'rate': [u'rate', u'charge_per_unit', u'pace', u'rank', u'range', u'order', u'grade', u'place', u'value']}
+synonym_dict['plot']=[u'plot',  u'diagram',   u'graph', u'figure']
 
+synonym_dict['show']=[u'show', u'display', u'appearance', u'demo', u'exhibit', u'present', u'demonstrate', u'prove', u'establish', u'shew', u'testify', u'bear_witness', u'evidence', u'picture', u'depict', u'render', u'express', u'evince', u'indicate', u'point', u'designate', u'show_up', u'read', u'register', u'record', u'usher']
 
+synonym_dict['hydrocarbon']=[u'oil']
 
-{'plot': [u'plot', u'secret_plan', u'game', u'plot_of_land', u'plot_of_ground', u'patch', u'diagram', u'plat']}
-
-
-{'show': [u'show', u'display', u'appearance', u'demo', u'exhibit', u'present', u'demonstrate', u'prove', u'establish', u'shew', u'testify', u'bear_witness', u'evidence', u'picture', u'depict', u'render', u'express', u'evince', u'indicate', u'point', u'designate', u'show_up', u'read', u'register', u'record', u'usher']}
+#synonym_dict['normally']=
